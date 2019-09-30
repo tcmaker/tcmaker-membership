0d3995c87cc674f7a031827610e4c1f1c9aff929
Stephen Van Dahm <code@vandahm.com>
2019-09-30 00:01:11 -0500

    membership info on dashboard

--- app/controllers/dashboard/index_controller.rb
+++ app/controllers/dashboard/index_controller.rb
@@ -1,4 +1,8 @@
 module Dashboard
   class IndexController < AuthenticatedController
+    def index
+      customer = Stripe::Customer.retrieve(current_member.stripe_identifier)
+      @subscription = customer.subscriptions.first
+    end
   end
 end
