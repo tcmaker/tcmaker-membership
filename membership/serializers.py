0153e05bebbc0e36f3a9b31b138654594b385b23
Stephen Van Dahm <code@vandahm.com>
2021-07-15 19:05:41 -0500

    keyfobs in person record

--- membership/serializers.py
+++ membership/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework.serializers import HyperlinkedModelSerializer
 from .models import Person, Household, DuesPlan, StudentTeam
 
+from keyfobs.models import KeyfobSerializer
+
 class HouseholdSerializer(HyperlinkedModelSerializer):
     class Meta:
         model = Household
@@ -24,6 +26,7 @@ class StudentTeamSerializer(HyperlinkedModelSerializer):
 class PersonSerializer(HyperlinkedModelSerializer):
     administered_households = HouseholdSerializer(many=True, read_only=True, source='administered_household_set')
     administered_student_teams = StudentTeamSerializer(many=True, read_only=True, source='administered_studentteam_set')
+    keyfobs = KeyfobSerializer(many=True, read_only=True, source='keyfob_set')
     class Meta:
         model = Person
         exclude = ['notes']
