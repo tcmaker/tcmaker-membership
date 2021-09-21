e5bbcad2d662ac103164150ca49f73cd92842514
Stephen Van Dahm <code@vandahm.com>
2021-09-20 21:45:33 -0500

    typo

--- keyfobs/serializers.py
+++ keyfobs/serializers.py
@@ -2,8 +2,8 @@ from rest_framework import serializers
 from .models import Keyfob
 
 class KeyfobSerializer(serializers.HyperlinkedModelSerializer):
-    is_membership_valid = serializers.BooleanField(source='compute_is_membership_valid')
-    membership_valid_through = serializers.DateTimeField(source='compute_membership_valid_through')
+    is_membership_valid = serializers.BooleanField(source='compute_is_membership_valid', required=False)
+    membership_valid_through = serializers.DateTimeField(source='compute_membership_valid_through', required=False)
 
     class Meta:
         model = Keyfob
